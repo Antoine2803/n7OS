5b845217b3df570b5b4f3c709c286eee398e6c77
Antoine2803 <82040673+Antoine2803@users.noreply.github.com>
2025-03-19 15:44:03 +0100

    feat: beautifull timer

--- kernel/timer.c
+++ kernel/timer.c
@@ -1,9 +1,20 @@
 #include <n7OS/timer.h>
 #include <n7OS/cpu.h>
+#include <n7OS/handler.h>
+
+extern uint32_t curr_time;
 
 
 void init_timer()
 {
+    // outb(0x0, CMOS_ADDRESS);
+    // int second = inl(CMOS_DATA);
+    // outb(0x2, CMOS_ADDRESS);
+    // int minute = inl(CMOS_DATA);
+    // outb(0x4, CMOS_ADDRESS);
+    // int hour = inl(CMOS_DATA);
+    // curr_time = curr_time + hour*60*60*1000 + minute*60*1000 + second*1000;
+
     uint32_t frequence = 1190;
 
     // Configure le timer
